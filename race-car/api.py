240eda3e64591c87f318f65c65d12ce50229761c
LucasLista <32399306+LucasLista@users.noreply.github.com>
2025-08-01 20:30:29 +0900

    Api set up for testing

--- race-car/api.py
+++ race-car/api.py
@@ -1,9 +1,18 @@
 import time
 import uvicorn
 import datetime
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Request
 from dtos import RaceCarPredictRequestDto, RaceCarPredictResponseDto
-from test_endpoint import return_action
+import random
+
+# from test_endpoint import return_action
+def return_action(state):
+    # Returns a list of actions
+    actions = []
+    action_choices = ['ACCELERATE', 'DECELERATE', 'STEER_LEFT', 'STEER_RIGHT', 'NOTHING']
+    for _ in range(10):
+        actions.append(random.choice(action_choices))
+    return actions
 
 HOST = "0.0.0.0"
 PORT = 9052
@@ -13,11 +22,10 @@ app = FastAPI()
 start_time = time.time()
 
 @app.post('/predict', response_model=RaceCarPredictResponseDto)
-def predict(request: RaceCarPredictRequestDto = Body(...)):
-    action = return_action(request.dict())
+async def predict(request: RaceCarPredictRequestDto = Body(...)):
+    action = return_action(request.model_dump())
     return RaceCarPredictResponseDto(
-        action_type=action['action_type'],
-        actions=action['actions']
+        actions=action
     )
 
 @app.get('/api')
@@ -31,9 +39,6 @@ def hello():
 @app.get('/')
 def index():
     return "Your endpoint is running!"
-
-
-
 
 if __name__ == '__main__':
 
