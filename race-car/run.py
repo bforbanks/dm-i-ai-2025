6a7a8952d0dfd46c05c9ac43258eac30ba37b143
Viktor Dyrby Larsen <s234857@dtu.dk>
2025-08-01 16:46:44 +0200

    A file slightly different from example.py

---
+++ race-car/run.py
@@ -0,0 +1,21 @@
+import pygame
+import random
+from src.game.core import initialize_game_state, game_loop
+from models.baseline import BaselineModel as MODEL
+
+'''
+Set seed_value to None for random seed.
+Within game_loop, change get_action() to your custom models prediction for local testing and training.
+'''
+
+
+
+
+
+if __name__ == '__main__':
+    mdl = MODEL()
+    seed_value = 12345
+    pygame.init()
+    initialize_game_state("http://example.com/api/predict", seed_value)
+    game_loop(verbose=True, model=mdl) # For pygame window
+    pygame.quit()
